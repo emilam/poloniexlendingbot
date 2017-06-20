dbfffbaa393faa70bd977907bb0edf43071782fc
Evrhet Milam <evrhet@utl.wtf>
2017-06-20 10:04:07 -0700

    Get proper gaptop and gapbottom when user has no gapmode set. Fixes #384

--- modules/Configuration.py
+++ modules/Configuration.py
@@ -93,6 +93,7 @@ def get_coin_cfg():
                     coin_cfg[cur]['gapmode'] = get_gap_mode(cur, 'gapmode')
                     coin_cfg[cur]['gapbottom'] = Decimal(get(cur, 'gapbottom', False, 0))
                     coin_cfg[cur]['gaptop'] = Decimal(get(cur, 'gaptop', False, coin_cfg[cur]['gapbottom']))
+                    import pdb;pdb.set_trace()
 
                 except Exception as ex:
                     ex.message = ex.message if ex.message else str(ex)
